bf1e0eb7bd3fd9fba15cf58d2632bd33f6b34e72
JessicaThi <jessica.thielemans@hetic.net>
2020-07-17 22:33:18 +0200

    test bis

--- day-13-sick-page-transition/index.js
+++ day-13-sick-page-transition/index.js
@@ -1,28 +1,29 @@
-//thanks to Codicts for his tutorial : https://www.youtube.com/watch?v=aMucZErEdZg&t=323s
+//thanks to DesignCourse for his tutorial : https://www.youtube.com/watch?v=aMucZErEdZg&t=323s
 
 function pageTransition() {
   var tl = gsap.timeline();
 
-  tl.to(".transition li", {
+  tl.to("ul.transition li", {
     duration: 0.5,
     scaleY: 1,
     transformOrigin: "bottom left",
     stagger: 0.2,
   });
-  tl.to(".transition li", {
+  tl.to("ul.transition li", {
     duration: 0.5,
     scaleY: 0,
     transformOrigin: "bottom left",
     stagger: 0.1,
+    delay: 0.1,
   });
 }
 
 function contentAnimation() {
   var tl = gsap.timeline();
   tl.from(".left", { duration: 1.5, translateY: 50, opacity: 0 });
-  tl.to(
+  tl.from(
     "img",
-    { clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%" },
+    { clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" },
     "-=1.1"
   );
 }
@@ -30,7 +31,7 @@ function contentAnimation() {
 function delay(n) {
   n = n || 2000;
   return new Promise((done) => {
-    setTimeOut(() => {
+    setTimeout(() => {
       done();
     }, n);
   });
@@ -38,6 +39,7 @@ function delay(n) {
 
 barba.init({
   sync: true,
+
   transitions: [
     {
       async leave(data) {
@@ -51,7 +53,6 @@ barba.init({
       async enter(data) {
         contentAnimation();
       },
-
       async once(data) {
         contentAnimation();
       },
